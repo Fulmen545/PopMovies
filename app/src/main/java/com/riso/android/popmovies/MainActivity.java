38930f8b0076a9ba81490e48f152148a8abd88eb
Fulmen545 <Fulmen999>
2017-10-30 13:28:28 +0100

    test commit

--- app/src/main/java/com/riso/android/popmovies/MainActivity.java
+++ app/src/main/java/com/riso/android/popmovies/MainActivity.java
@@ -57,6 +57,8 @@ public class MainActivity extends AppCompatActivity {
 
         }*/
 
+       //tesst
+
        if (isOnline()) {
            FrameLayout frame = new FrameLayout(this);
            frame.setId(android.R.id.content);
